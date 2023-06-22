e3ea66b519b99c7f7e3c3d4f8f7676bcd775885b
helloa1109 <helloa1109@naver.com>
2023-06-22 20:58:29 +0900

    test

---
+++ src/main/java/com/example/dopeboyzclub/DopeboyzclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.dopeboyzclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopeboyzclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopeboyzclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dopeboyzclub/DopeboyzclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dopeboyzclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopeboyzclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
